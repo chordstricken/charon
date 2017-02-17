93f899a931d27bb6ad2bae22e22d0074eabbb574
Jason <jason@invexi.com>
2017-02-17 10:57:51 -0700

    added 403 and 404 pages

---
+++ html/403.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="/img/favicon.png" />
+
+    <title>Charon - 403: Forbidden</title>
+
+    <link href="/src/css/build.css" rel="stylesheet">
+    <link href="/src/css/errorpage.css" rel="stylesheet">
+</head>
+<body>
+
+    <div class="text-center">
+        <h1>403</h1>
+        <?=(isset($data) ? $data : '')?>
+
+    </div>
+
+</body>
+</html>
