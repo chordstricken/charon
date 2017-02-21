7e7251531fc7512712b5816a8a2f7a2b7436ad2d
Jason <jason@invexi.com>
2017-02-20 21:12:21 -0700

    Added api\Account

---
+++ lib/api/Account.php
@@ -0,0 +1,53 @@
+<?php
+namespace api;
+
+use core;
+use models;
+use \Exception;
+use \stdClass;
+
+/**
+ * /account API path
+ * @author Jason Wright <jason@silvermast.io>
+ * @since 1/3/17
+ * @package charon
+ */
+class Account extends core\APIRoute {
+
+    protected $is_encrypted = true;
+
+    /**
+     * GET /account
+     * Reads the authenticated user's account
+     */
+    public function get() {
+        if (!$this->is_json) {
+            require(HTML . '/account.php');
+            die();
+
+        } else {
+            // load the locker data object
+            $this->send(models\Account::current());
+
+        }
+    }
+
+    /**
+     * POST /account
+     * Saves the user's account information
+     * @throws Exception
+     */
+    public function post() {
+        if (!$this->data instanceof stdClass)
+            throw new Exception('Invalid Request Object', 400);
+
+        unset($this->data->id); // avoid spoofing user id
+        unset($this->data->slug);
+
+        if ($obj = models\Account::current())
+            $obj->setVars($this->data)->validate()->save();
+
+        $this->send($obj);
+    }
+
+}
